f6223fa408f12a97424ec6396a348cfa4fc746a7
Logan Brown <logan.brown@tylertech.com>
2025-03-24 12:46:04 -0400

    Update error logging for http api

--- api/http.ts
+++ api/http.ts
@@ -22,6 +22,7 @@ export default class HTTP {
     const headers = result.headers;
 
     if (!result.ok) {
+      console.log(url.toString());
       console.log(headers);
       throw new Error(`Error ${result.status}: ${data.message}`);
     }
