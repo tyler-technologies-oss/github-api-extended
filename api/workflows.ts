fc499f340890d01f184333615df7cdd842626bea
Logan Brown <logan.brown@tylertech.com>
2025-10-20 14:55:52 -0400

    Update workflow return properties

--- api/workflows.ts
+++ api/workflows.ts
@@ -3,13 +3,13 @@ import HTTP from "./http";
 export default class WorkflowAPI {
   constructor(private httpservice: HTTP, private config: IGitHubApi) {}
 
-  public get(): Promise<IObject> {
+  public async get(): Promise<IObject[]> {
     const url = new URL(
       `${this.config.url}/repos/${this.config.repository?.owner}/${this.config.repository?.name}/actions/workflows`
     );
-    const data = this.httpservice.get(url);
-    const workflows = (data as any).workflows;
-    return workflows;
+
+    const response = await this.httpservice.get(url);
+    return response?.data?.workflows || [];
   }
 
   public dispatch(id: number, body?: Body): Promise<IObject> {
